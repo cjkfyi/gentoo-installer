4df257f571038b11bbcd70689810c1881cd843b6
cjkfyi <165058376+cjkfyi@users.noreply.github.com>
2024-07-13 01:30:12 -0700

    testing use_flags()

--- chroot.sh
+++ chroot.sh
@@ -7,7 +7,7 @@ echo "ECHO FROM CHROOT"
 #
 
 function make_conf() {
-    
+
     COMMON_FLAGS="-march=alderlake -mabm -mno-cldemote -mno-kl -mno-sgx -mno-widekl -mshstk --param=l1-cache-line-size=64 --param=l1-cache-size=48 -O2 -pipe"
 
     MAKEOPTS="-j12 -l12"
@@ -26,49 +26,73 @@ function make_conf() {
 
     # Generate the make.conf file
     cat > /etc/portage/make.conf <<EOF
-    # These settings were set by the catalyst build script that automatically
-    # built this stage.
-    # Please consult /usr/share/portage/config/make.conf.example for a more
-    # detailed example.
+COMMON_FLAGS="${COMMON_FLAGS}"
+CFLAGS="\${COMMON_FLAGS}"
+CXXFLAGS="\${COMMON_FLAGS}"
+FCFLAGS="\${COMMON_FLAGS}"
+FFLAGS="\${COMMON_FLAGS}"
 
-    COMMON_FLAGS="${COMMON_FLAGS}"
-    CFLAGS="${COMMON_FLAGS}"
-    CXXFLAGS="${COMMON_FLAGS}"
-    FCFLAGS="${COMMON_FLAGS}"
-    FFLAGS="${COMMON_FLAGS}"
+MAKEOPTS="${MAKEOPTS}"
+PORTAGE_NICENESS=${PORTAGE_NICENESS}
 
-    MAKEOPTS="${MAKEOPTS}"
-    PORTAGE_NICENESS=${PORTAGE_NICENESS}
+EMERGE_DEFAULT_OPTS="--ask --tree --verbose --jobs=12 --load-average=12 --with-bdeps y --complete-graph y"
+FEATURES="candy fixlafiles unmerge-orphans parallel-fetch parallel-install"
 
-    EMERGE_DEFAULT_OPTS="--ask --tree --verbose --jobs=12 --load-average=12 --with-bdeps y --complete-graph y"
-    FEATURES="candy fixlafiles unmerge-orphans parallel-fetch parallel-install"
+CPU_FLAGS_X86="aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sha sse sse2 sse3 sse4_1 sse4_2 ssse3 vpclmulqdq"
 
-    CPU_FLAGS_X86="aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sha sse sse2 sse3 sse4_1 sse4_2 ssse3 vpclmulqdq"
+ACCEPT_LICENSE="*"
+ACCEPT_KEYWORDS="~amd64"
+GRUB_PLATFORMS="efi-64"
+VIDEO_CARDS="nvidia"
+INPUT_DEVICES="libinput"
 
-    ACCEPT_LICENSE="*"
-    ACCEPT_KEYWORDS="~amd64"
-    GRUB_PLATFORMS="efi-64"
-    VIDEO_CARDS="nvidia"
-    INPUT_DEVICES="libinput"
+PORTAGE_TMPDIR="/var/tmp/portage"
+PORTAGE_SCHEDULING_POLICY="idle"
 
-    PORTAGE_TMPDIR="/var/tmp/portage"
-    PORTAGE_SCHEDULING_POLICY="idle"
+# NOTE: This stage was built with the bindist Use flag enabled
 
-    # NOTE: This stage was built with the bindist Use flag enabled
+PORTDIR="${PORTDIR}"
+DISTDIR="${DISTDIR}"
+PKGDIR="${PKGDIR}"
 
-    PORTDIR="${PORTDIR}"
-    DISTDIR="${DISTDIR}"
-    PKGDIR="${PKGDIR}"
+# Set language of build output to English.
+LC_MESSAGES=${LC_MESSAGES}
+LANG=${LANG}
+L10N=${L10N}
 
-    # Set language of build output to English.
-    LC_MESSAGES=${LC_MESSAGES}
-    LANG=${LANG}
-    L10N=${L10N}
-
-    GENTOO_MIRRORS="${GENTOO_MIRRORS}"
+GENTOO_MIRRORS="${GENTOO_MIRRORS}"
 EOF
+    printf "make.conf was set"
 }
 
 if ! make_conf; then
+    exit 1
+fi
+
+#
+
+function use_flags() {
+
+    # Generate the make.conf file
+    cat > /etc/portage/package.use/\* <<EOF
+#################################### GL0BAL ####################################
+#
+# Enabled
+#
+*/* minimal dist-kernel elogind dbus udev udisks python_targets_python3_12
+*/* bash-completion jpeg jpegxl heif gif png opengl nvenc vaapi vulkan
+*/* pulseaudio wayland X
+#
+# Disabled
+#
+*/* -python_targets_python2_7 -systemd -ipv6 -nouveau -kde -gnome -consolekit
+*/* -cups -crypt
+#################################### L0CAL #####################################
+EOF
+
+    printf "use flags set"
+}
+
+if ! use_flags; then
     exit 1
 fi
